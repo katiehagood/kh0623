c5e85e2d5fa269564a30de3d7e76790109b6d0b4
Katie Hagood <katiehagood04@gmail.com>
2023-06-25 19:54:11 -0400

    Adds command line interface

--- cooltools/src/main/java/com/katiehagood/App.java
+++ cooltools/src/main/java/com/katiehagood/App.java
@@ -1,13 +1,64 @@
 package com.katiehagood;
 
+import java.util.Scanner;
+
+import com.katiehagood.exceptions.CheckoutException;
+import com.katiehagood.model.CheckoutCart;
+import com.katiehagood.model.RentalAgreement;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    public static CheckoutCart getCheckoutInfo(Scanner scanner) {
+        System.out.println("Please enter your rental info");
+        
+        System.out.println("Tool Code:");
+        String toolCode = scanner.nextLine();
+
+        System.out.println("Rental day count:");
+        int rentalDayCount = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Discount Percent:");
+        int discount = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Check Out Date:");
+        String checkOutDate = scanner.nextLine();
+
+        try {
+            return new CheckoutCart(toolCode, rentalDayCount, discount, checkOutDate);
+        } catch (CheckoutException checkoutException) {
+            System.out.println("Checkout unable to be completed.");
+            System.out.println("Reason: " + checkoutException.getMessage());
+        }
+        return null;
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Welcome to Cool Tools!");
+
+        boolean active = true;
+        Scanner scanner = new Scanner(System.in);
+        CheckoutCart cart = getCheckoutInfo(scanner);
+
+        while (active && cart == null) {
+            System.out.println("Would you like to try again? (y/n)");
+            String answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("y")) {
+                cart = getCheckoutInfo(scanner);
+            } else {
+                active = false;
+            }
+        }
+
+        if (cart != null) {
+            RentalAgreement ra = cart.generateRentalAgreement();
+            System.out.println(ra.toString());
+        }
+
+        scanner.close();
     }
 }
